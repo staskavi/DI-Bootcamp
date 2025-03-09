0b032bd9ce2ff8ed99a8d4b3853a5e00168cf77d
StasK <staskavi@gmail.com>
2025-03-09 23:12:14 +0200

    Ex1

---
+++ pythonW1D1.py
@@ -0,0 +1,18 @@
+#Exercise 1 : Hello World
+#Instructions
+
+#Print the following output in one line of code:
+
+#Hello world
+#Hello world
+#Hello world
+#Hello world
+print("Hello world "+"Hello world "+"Hello world "+"Hello world "+"Hello world ")
+print("Hello world","Hello world","Hello world","Hello world","Hello world")
+hw = "Hello world "
+print(hw*5)
+
+#Exercise 2 : Some Math
+#Instructions
+
+#Write code that calculates the result of: (99^3)*8 (meaning 99 to the power of 3, times 8).
