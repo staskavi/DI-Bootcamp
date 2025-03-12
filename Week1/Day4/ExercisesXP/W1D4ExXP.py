d5b6c00d660ff0fa7ec204127acbfe2951b8c4c5
StasK <staskavi@gmail.com>
2025-03-13 01:42:21 +0200

    finished ExXP

--- Week1/Day4/ExercisesXP/W1D4ExXP.py
+++ Week1/Day4/ExercisesXP/W1D4ExXP.py
@@ -118,24 +118,77 @@ else:
 #     Write a loop that asks a user to enter a series of pizza toppings, when the user inputs ‘quit’ stop asking for toppings.
 #     As they enter each topping, print a message saying you’ll add that topping to their pizza.
 #     Upon exiting the loop print all the toppings on the pizza pie and what the total price is (10 + 2.5 for each topping).
-quit_word = "quit"
-your_input = ""
-topping_list = []
-topping_counter = 0
-loop_counter = 0
-total_price = 0
-while(1):
-     if(your_input.upper() == quit_word.upper()):
-      print("All the toppings you choose are:")
-      topping_list= topping_list[:-1]
-      print(topping_list)
-      total_price = ((topping_counter-1)*2.5)+10
-      print("Total price is "+ str(total_price))
-      break
-     else:
-       print("Enter pizza topping")
-       your_input = input()
-       topping_list.insert(loop_counter,your_input.split())
-       loop_counter+=1
-       topping_counter+=1
-   
+# quit_word = "quit"
+# your_input = ""
+# topping_list = []
+# topping_counter = 0
+# loop_counter = 0
+# total_price = 0
+# while(1):
+#      if(your_input.upper() == quit_word.upper()):
+#       print("All the toppings you choose are:")
+#       topping_list= topping_list[:-1]
+#       print(topping_list)
+#       total_price = ((topping_counter-1)*2.5)+10
+#       print("Total price is "+ str(total_price))
+#       break
+#      else:
+#        print("Enter pizza topping")
+#        your_input = input()
+#        topping_list.insert(loop_counter,your_input.split())
+#        loop_counter+=1
+#        topping_counter+=1
+
+#    Exercise 9: Cinemax
+# Instructions
+#     A movie theater charges different ticket prices depending on a person’s age.
+#         if a person is under the age of 3, the ticket is free.
+#         if they are between 3 and 12, the ticket is $10.
+#         if they are over the age of 12, the ticket is $15.
+#     Ask a family the age of each person who wants a ticket.
+#     Store the total cost of all the family’s tickets and print it out.
+#     A group of teenagers are coming to your movie theater and want to watch a movie that is restricted for people between the ages of 16 and 21.
+#     Given a list of names, write a program that asks teenager for their age, if they are not permitted to watch the movie, remove them from the list.
+#     At the end, print the final list.
+
+
+# print("How old are you?")
+# age = input()
+# clients_age_list = []  #didn't understand structure of list. Is it must include names and ages? When program should stop asking the age?  What is " Given a list of names"?
+# # where is it? not clear at all
+# total_tickets_price = 0
+# if(age < 3):
+#  total_tickets_price+=0
+# elif(age>3 and age <12):
+#    total_tickets_price+=10
+# else:
+#    total_tickets_price+=15 
+
+
+
+
+# Exercise 10 : Sandwich Orders
+# Instructions
+# Using the list below :
+# sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+
+#     The deli has run out of pastrami, use a while loop to remove all occurrences of “Pastrami sandwich” from sandwich_orders.
+#     We need to prepare the orders of the clients:
+#         Create an empty list called finished_sandwiches.
+#         One by one, remove each sandwich from the sandwich_orders while adding them to the finished_sandwiches list.
+#     After all the sandwiches have been made, print a message listing each sandwich that was made
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+finished_sandwiches = []
+run_out = "Pastrami sandwich"
+print(sandwich_orders)
+while(run_out in sandwich_orders):
+   sandwich_orders.remove(run_out)
+print(sandwich_orders) 
+for i in range (len(sandwich_orders)):
+ finished_sandwiches.append(sandwich_orders[i])
+
+print(finished_sandwiches)
+sandwich_orders.clear()
+print(sandwich_orders)
+for i in range(len(finished_sandwiches)):
+   print("I made your ",finished_sandwiches[i])
