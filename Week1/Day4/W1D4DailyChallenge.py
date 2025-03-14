8713f9fb29ceea39741ddbb6c3af8c19c89ccd57
StasK <staskavi@gmail.com>
2025-03-15 01:56:12 +0200

    W1D4 Daily Challenge Complited

---
+++ Week1/Day4/W1D4DailyChallenge.py
@@ -0,0 +1,52 @@
+# Challenge 1
+
+#     Ask the user for a number and a length.
+#     Create a program that prints a list of multiples of the number until the list length reaches length.
+
+print("Enter number...")
+user_num = int(input())
+print("Enter length...")
+user_len = int(input())
+user_list = []
+multiple_user_num = user_num
+while(user_len):
+    user_list.append(multiple_user_num)
+    multiple_user_num += user_num
+    user_len-=1
+print(user_list) 
+
+# Challenge 2
+
+#     Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+
+# Examples
+
+# user's word : "ppoeemm" ➞ "poem"
+
+# user's word : "wiiiinnnnd" ➞ "wind"
+
+# user's word : "ttiiitllleeee" ➞ "title"
+
+# user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
+
+# Notes
+# Final strings won’t include words with double letters (e.g. “passing”, “lottery”).   
+print("Enter string...")
+user_str = input()
+formatted_str = user_str
+a = list(formatted_str)
+new_list = []
+new_list.append(a[0])
+i = 0
+j=0
+k=len(user_str)-1
+while(k):
+     if(new_list[j]!=a[i+1]):
+         new_list.insert(j+1,a[i+1])
+         j+=1
+     i+=1
+     k-=1
+formatted_str =  "".join(new_list)   
+print(formatted_str)
+
+
