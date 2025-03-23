016938a1044b4e5e5612c0ff2d3eb013dc90eabd
StasK <staskavi@gmail.com>
2025-03-23 23:27:42 +0200

    Timed Challenge1

---
+++ Week2/Day4/TimedChallenge1/TimedChallenge1.py
@@ -0,0 +1,14 @@
+
+# Reverse the Sentence
+
+# Write a program to reverse the sentence wordwise.
+
+# Input:
+# You have entered a wrong domain
+# Output:
+# domain wrong a entered have You
+
+sentence = input("Enter your sentence..")
+print(sentence)
+reversed = sentence[::-1]
+print(reversed)
