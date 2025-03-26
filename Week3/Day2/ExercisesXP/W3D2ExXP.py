9f009a4acfb4cc8887905931ffa26843bf08a871
StasK <staskavi@gmail.com>
2025-03-27 00:19:03 +0200

    W3D2 ExXP

--- Week3/Day2/ExercisesXP/W3D2ExXP.py
+++ Week3/Day2/ExercisesXP/W3D2ExXP.py
@@ -83,5 +83,59 @@ size = input("Please, enter a size...")
 txt = input("Please, enter a message you want on a T-shirt...")
 make_shirt(size, txt)
 
+# Exercise 6 : Magicians …
+# Instructions
 
-          
+# Using this list of magician’s names
+
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+#     Create a function called show_magicians(), which prints the name of each magician in the list.
+#     Write a function called make_great() that modifies the original list of magicians by adding the phrase "the Great" to each magician’s name.
+#     Call the function make_great().
+#     Call the function show_magicians() to see that the list has actually been modified.
+
+magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+def show_magicians(magician_names):
+     for names in magician_names:
+      print(names)
+show_magicians(magician_names)
+
+def make_great(magician_names):
+    i = 0
+    for names in magician_names:
+        names += " the Great"
+        magician_names[i] = names
+        i+=1
+make_great(magician_names)
+show_magicians(magician_names)
+
+#  Exercise 7 : Temperature Advice
+# Instructions
+
+#     Create a function called get_random_temp().
+#         This function should return an integer between -10 and 40 degrees (Celsius), selected at random.
+#         Test your function to make sure it generates expected results.
+
+#     Create a function called main().
+#         Inside this function, call get_random_temp() to get a temperature, and store its value in a variable.
+#         Inform the user of the temperature in a friendly message, eg. “The temperature right now is 32 degrees Celsius.”
+
+#     Let’s add more functionality to the main() function. Write some friendly advice relating to the temperature:
+#         below zero (eg. “Brrr, that’s freezing! Wear some extra layers today”)
+#         between zero and 16 (eg. “Quite chilly! Don’t forget your coat”)
+#         between 16 and 23
+#         between 24 and 32
+#         between 32 and 40
+
+#     Change the get_random_temp() function:
+#         Add a parameter to the function, named ‘season’.
+#         Inside the function, instead of simply generating a random number between -10 and 40, set lower and upper limits based on the season, eg. if season is ‘winter’, temperatures should only fall between -10 and 16.
+#         Now that we’ve changed get_random_temp(), let’s change the main() function:
+#             Before calling get_random_temp(), we will need to decide on a season, so that we can call the function correctly. Ask the user to type in a season - ‘summer’, ‘autumn’ (you can use ‘fall’ if you prefer), ‘winter’, or ‘spring’.
+#             Use the season as an argument when calling get_random_temp().
+
+#     Bonus: Give the temperature as a floating-point number instead of an integer.
+#     Bonus: Instead of asking for the season, ask the user for the number of the month (1 = January, 12 = December). Determine the season according to the month.
+
+
